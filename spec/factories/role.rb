084fab3af922818de85fe3ccd163b53a2505f88f
Dmitry Dimitrenko <dimitrenko.da@gmail.com>
2020-11-05 20:29:53 +0300

    Add factories

---
+++ spec/factories/role.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :role, class: Models::Role do
+    trait :admin do
+      initialize_with { Models::Role.find_or_create_by(code: 'admin', title: 'Администратор') }
+    end
+
+    trait :buyer do
+      initialize_with { Models::Role.find_or_create_by(code: 'buyer', title: 'Покупатель') }
+    end
+
+    trait :seller do
+      initialize_with { Models::Role.find_or_create_by(code: 'seller', title: 'Продавец') }
+    end
+  end
+end
